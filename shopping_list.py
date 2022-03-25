bb61e03090af2e5fb7292050b3c1fe824800976c
Łukasz Wira <lukasz.wira@gmail.com>
2022-03-25 22:29:32 +0100

    Add items counting

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,7 @@ sklepy = {
     "piekarnia": ['Chleb', 'Pączek', 'Bułki'],
     "warzywniak": ['Marchew', 'Seler', 'Rukola']
 }
+produkty = 0
+for shops in sklepy:
+    produkty += len(sklepy[shops])
+    
