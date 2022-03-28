cd49197d9c5ace2f2504857b1eda7a60c321b30e
Łukasz Wira <lukasz.wira@gmail.com>
2022-03-28 23:12:02 +0200

    add greeting print

--- shopping_list.py
+++ shopping_list.py
@@ -8,3 +8,4 @@ for shop in shops:
     print("idę do", shop.upper(), "i kupuję tam", ', '.join(shops[shop]).upper())
 print("W sumie kupuję", products,"produktów")
 print("nowy commit na zdalne repo")
+print("pozdrowienia od kolektywu mieszkańców bloku ćwiartki 3/4")
